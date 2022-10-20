ede6d134daf2e8b79d56434494236c5b6e5125fc
nanonachino-522 <60164853+nanonachino-522@users.noreply.github.com>
2022-10-21 05:52:18 +0700

    add ReciveOTP and Forget Password

--- src/main/java/senior/project/firework/repositories/repoOTP.java
+++ src/main/java/senior/project/firework/repositories/repoOTP.java
@@ -1,7 +1,13 @@
 package senior.project.firework.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import senior.project.firework.models.OTP;
 
+import java.util.List;
+
 public interface repoOTP extends JpaRepository<OTP,Long> {
+
+    @Query(value = "select max(o.idOtp) from OTP o,Account a where o.account.idAccount = a.idAccount and a.idAccount = ?1 ")
+    long getMaxIdOTP(long idAccount);
 }
